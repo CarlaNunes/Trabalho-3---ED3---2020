500435978e645e9a8b69a6d6628b29d1002eed95
CarlaNunes <carla.nunes@usp.br>
2020-11-12 15:30:14 -0300

    Create cria_Index.c

---
+++ cria_Index.c
@@ -0,0 +1,50 @@
+void cria_Index(FILE *arquivo,int cont,char nomeArquivoIndice[20]){
+ 
+  char status = '0'; //Arquivo de dados consistente. Ao finalizar terá valor igual a -1.
+  char lixo[] = "$$$$$$$";
+  char linha[400];
+  int id = 0;
+  int aux = 0;
+  //nt rRN = 0;
+  int i = 0;
+  if ((arquivo = fopen(nomeArquivoIndice,"wb")) == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    }
+  
+  
+  else{
+      //As duas linhas abaixo, escrevem o cabeçalho do arquivo de indíce primário.
+      fwrite(&status, sizeof(char) , 1 , arquivo);
+      fwrite(lixo, sizeof(char) , 1,  arquivo);
+      //Testando os valores recebidos:
+       for(i = 0; i < cont; i++){
+        // printf("\n ID: %d \n",index_p[i].idPessoa);
+         //printf("\n RRN: %d \n",index_p[i].rrn);
+       }
+     
+      for(i = 0; i < cont; i++){
+         fwrite(&pessoa,1, sizeof(Pessoa),arquivo);
+      }
+     // printf("##### INSERINDO NO INDEX #####\n");
+      //printf("ID: %d\n", auxIndex.numIUSP);
+      //printf("RRN: %d\n", auxIndex.rrn);
+    //auxIndex.rrn++;
+      //printf("############################\n");
+
+   
+    //fwrite (&auxIndex, sizeof(indice), 1, df2);
+
+ 
+     // }
+    //auxIndex.tam = (auxrrn-1)*72;
+      
+   
+ 
+
+      
+      
+  }
+
+  fclose(arquivo);
+  
+}
