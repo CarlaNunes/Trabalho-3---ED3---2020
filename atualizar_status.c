c3e242a0cec02b735d206a84664741e33fc02f55
CarlaNunes <carla.nunes@usp.br>
2020-11-15 19:34:36 -0300

    Create atualizar_status.c

---
+++ atualizar_status.c
@@ -0,0 +1,67 @@
+/*
+ A função abaixo, atualiza o status do cabeçalho quando se finaliza a operação.
+ */
+void atualizar_status(FILE *arquivo,int cont,char nomeArquivoBinario[20]){
+   char status = '1';
+  char nome[40];
+  int quantidadePessoas = cont;
+  char lixo[] = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$";
+
+  if ((arquivo = fopen(nomeArquivoBinario, "wb")) == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    }
+     // fprintf(arquivo,"%s%d%s",&status,quantidadePessoas,lixo);
+     fwrite(&status,sizeof(char) ,1, arquivo);
+     fwrite(&quantidadePessoas,sizeof(int) ,1, arquivo);
+     fwrite(&lixo, sizeof(char) ,1, arquivo);
+
+    for(int i = 0; i<cont; i++){
+      //A parte de complementamndo com lixos não deu certo.
+      
+      //Completando as strings com lixo
+      /*
+      if(strlen(pessoa[i].pessoa)<40){
+          int tamFinal = 40 - strlen(pessoa[i].pessoa);
+          for(int j = tamFinal; j < 40; j++){
+            strcpy(pessoa[i].pessoa, "$"); 
+          }
+         
+        //fwrite(&pessoa, sizeof(Pessoa),1,arquivo);  
+      }
+      if(strlen(pessoa[i].twitterPessoa)<15){
+          int tamFinal = 15 - strlen(pessoa[i].pessoa);
+          for(int j = tamFinal; j < 15;j++){
+            strcpy(pessoa[i].twitterPessoa, "$"); 
+          }
+      }
+      */
+      //else{
+            int tam = 0, tam2 = 0;
+            fwrite(&removido,1,sizeof(char),arquivo);
+            int id = pessoa[i].idPessoa;
+            fwrite(&id,1,sizeof(int),arquivo);
+
+          //  if(strlen(pessoa[i].pessoa)<1){
+         // int tamFinal = 40 - strlen(pessoa[i].pessoa);
+         // for(int j = tamFinal; j < 40;j++){
+           //   char lixo = '$';
+             //  strcpy(pessoa[i].pessoa, "$"); 
+              // strcpy(nome,pessoa[i].pessoa);
+              // fwrite(&nome,1,sizeof(char),arquivo);
+             
+           // }
+           
+            strcpy(nome,pessoa[i].pessoa);
+            fwrite(&nome,1,sizeof(char),arquivo);
+            int idade = pessoa[i].idade;
+            fwrite(&idade,1,sizeof(int),arquivo);
+            char twitter[15];
+            strcpy(twitter,pessoa[i].pessoa);
+            fwrite(&twitter,1,sizeof(char),arquivo);
+            
+       
+    }
+
+
+    fclose(arquivo);
+ }
