9fed7483eade1a11fbfd93849f97b94d24786d36
CarlaNunes <carla.nunes@usp.br>
2020-11-15 18:13:13 -0300

    Update atualizacao_index.c

--- atualizacao_index.c
+++ atualizacao_index.c
@@ -1,4 +1,11 @@
 
+/*
+A função, integra a funcionalidade 5, que é a de atualização de registros.
+Quando o usúario (ou outro) inserir os dados que deseja atualizar, eles, são armazenados em uma struct auxiliar(auxiliar2)
+para que assim, seja trasnferido para a struct original e inserida no arquivo de indice primário.(index binário).
+
+*/
+
 void atualizacao_index(int cont, int numAtu){
  int i = 0;
   for(i = 0; i < cont; i++){
